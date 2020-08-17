18bb2f4b6667df346406bec549a909dc82f65576
yj <52307613+WhyJYJ@users.noreply.github.com>
2020-08-17 18:01:27 +0900

    #14.1 Form and CSRF

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name = "users"
 
-urlpatterns = [path("login", views.LoginView.as_view(), name="login")]
+urlpatterns = [
+    path("login", views.LoginView.as_view(), name="login"),
+]
 
