9c78d3194486fc93e3b96093bf2f6eda635baf96
yj <52307613+WhyJYJ@users.noreply.github.com>
2021-01-05 20:28:40 +0900

    #26.4 Postgres part Two

--- config/settings.py
+++ config/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "aa*jljh58yqdw*)0dnhk_t2f!((#yd=yx#$$t74j+i6)6dkhqu"
+SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG"))
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".elasticbeanstalk.com"]
 
 
 # Application definition
@@ -89,7 +89,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
-if DEBUG:
+if DEBUG is False:
 
     DATABASES = {
         "default": {
