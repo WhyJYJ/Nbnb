de579dbb63523cba5501a68ef94578f9e36693e1
yj <52307613+WhyJYJ@users.noreply.github.com>
2021-01-13 00:46:32 +0900

    [FIX] createsu by env info

--- users/management/commands/createsu.py
+++ users/management/commands/createsu.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from users.models import User
+import os
 
 
 class Command(BaseCommand):
@@ -7,9 +8,13 @@ class Command(BaseCommand):
     help = "This command creates superuser"
 
     def handle(self, *args, **options):
-        admin = User.objects.get_or_none(username="ebadmin")
+        admin = User.objects.get_or_none(username=os.environ.get("ADMIN_NAME"))
         if not admin:
-            User.objects.create_superuser("ebadmin", "kkksss@skku.edu", "123456")
+            User.objects.create_superuser(
+                os.environ.get("ADMIN_NAME"),
+                os.environ.get("ADMIN_EMAIL"),
+                os.environ.get("ADMIN_PW"),
+            )
             self.stdout.write(self.style.SUCCESS(f"Superuser Created"))
         else:
             self.stdout.write(self.style.SUCCESS(f"Superuser Exists"))
